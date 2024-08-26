9d3fdd06c76c3a86672a2b6215bb09910930c80c
GuradianOfData <mvrheitor@gmail.com>
2024-08-26 17:12:52 -0300

    Biblioteca de babel

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-// Chave criptografada usando um hash simples
-const chaveCriptografada = btoa("1,618033");
-
 function verificarChave() {
     const chaveDigitada = document.getElementById('chave').value;
     const chaveCriptografadaDigitada = btoa(chaveDigitada);
 
     if (chaveCriptografadaDigitada === chaveCriptografada) {
+        // Chave correta
         document.getElementById('mensagem').innerText = '';
         window.location.href = 'biblioteca.html';
     } else {
+        // Chave incorreta
         document.getElementById('mensagem').innerText = 'Chave incorreta! Tente novamente.';
     }
 }
 
 function buscarLivro() {
     const pesquisa = document.getElementById('pesquisa').value.toLowerCase();
-    const livros = document.querySelectorAll('#lista-livros a');
+    const categorias = document.querySelectorAll('.categoria div');
 
-    livros.forEach(function(livro) {
-        const titulo = livro.innerText.toLowerCase();
-        if (titulo.includes(pesquisa)) {
-            livro.style.display = 'block';
-        } else {
-            livro.style.display = 'none';
-        }
+    categorias.forEach(function(categoria) {
+        const livros = categoria.querySelectorAll('a');
+        let encontrado = false;
+
+        livros.forEach(function(livro) {
+            const titulo = livro.innerText.toLowerCase();
+            if (titulo.includes(pesquisa)) {
+                livro.style.display = 'block';
+                encontrado = true;
+            } else {
+                livro.style.display = 'none';
+            }
+        });
+
+        // Mostrar ou esconder a categoria com base no resultado da pesquisa
+        categoria.parentElement.style.display = encontrado ? 'block' : 'none';
     });
 }
-
-async function selecionarPastaLivros() {
-    try {
-        const dirHandle = await window.showDirectoryPicker();
-        const listaLivros = document.getElementById('lista-livros');
-        listaLivros.innerHTML = '';  // Limpar a lista anterior
-
-        for await (const [nomeArquivo, handle] of dirHandle.entries()) {
-            if (handle.kind === 'file' && nomeArquivo.endsWith('.pdf')) {
-                const link = document.createElement('a');
-                link.href = URL.createObjectURL(await handle.getFile());
-                link.innerText = nomeArq

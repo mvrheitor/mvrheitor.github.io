88e34b32662f5390eb44b8da3f43e8245d06bf3d
HR <mvrheitor@gmail.com>
2026-04-03 13:30:43 -0300

    Update script.js

--- Biblioteca/script.js
+++ Biblioteca/script.js
@@ -16,6 +16,7 @@ const books = [
     { title: "Meditações", href: "Livros/meditacoes marco aurelio.pdf" },
     { title: "Mais Esperto que o Diabo", href: "Livros/Mais-Esperto-que-o-Diabo-Napoleon-Hill_251218_103311.pdf" },
     { title: "O Que Todo Corpo Fala", href: "Livros/O_que_todo_corpo_fala_Joe_Navarro.pdf" },
+    { title: "O Corpo Fala", href: "Livros/O_Corpo_Fala_Pierre_Weil.pdf" },
 ];
 
 const normalize = (text) =>
